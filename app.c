97f3a4749f13f67dbb090a50c05794da3fcb140b
Muhammad Kashif <132649154+MuhammadKashif03@users.noreply.github.com>
2023-10-10 12:11:49 +0500

    size of datatypes in c

---
+++ app.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{ 
+ //enter user an integer value
+ int value;
+ printf("Enter value: ");
+ scanf("%d",&value);
+ printf("the integer value: %d ", value);
+
+    return 0;
+}
