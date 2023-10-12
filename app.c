18395cebf97952c91be1bcf9f423169f7440bca4
Muhammad Kashif <132649154+MuhammadKashif03@users.noreply.github.com>
2023-10-12 17:21:12 +0500

    input output multiple values

--- app.c
+++ app.c
@@ -1,19 +1,13 @@
 #include <stdio.h>
 int main()
 { 
-//Now user will enter float and double
-
-float num1;
-double num2;
-
-printf("enter float number :");
-scanf("%f",&num1);
-printf("enter double :");
-scanf("%lf",&num2);
-
-printf("float number : %f\n",num1);
-printf("double number : %lf",num2);
-
+//input output Multiple Values
+int a ;
+float b;
+printf("Enter multiple value");
+scanf("%d%f",&a,&b);
+printf("entered values is :%d \n",a);
+printf("entered values is :%f",b);
 
 return 0;
 }
