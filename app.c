5d66c16452648174e5176d1b1982feb769704a2c
Muhammad Kashif <132649154+MuhammadKashif03@users.noreply.github.com>
2023-10-12 17:56:15 +0500

    arithmetic operators in c

--- app.c
+++ app.c
@@ -1,13 +1,28 @@
 #include <stdio.h>
 int main()
-{ 
-//input output Multiple Values
-int a ;
-float b;
-printf("Enter multiple value");
-scanf("%d%f",&a,&b);
-printf("entered values is :%d \n",a);
-printf("entered values is :%f",b);
+{
+    //working of arithmetic operators
+    int a = 9,b = 4, c;
+    //sum of two numbers
+    c= a+b;
+    printf("sum is : %d\n" , c);
 
-return 0;
+    //minus of given number
+    c= a-b;
+    printf("minus is : %d\n",c);
+
+    //multiplication 
+    c= a*b;
+    printf("multiplication is : %d\n", c);
+
+    //division 
+    c= a/b;
+    printf("division is : %d\n", c);
+
+    //reminder
+    c= a%b;
+    printf("reminder is : %d\n", c);
+  
+    return 0;
+
 }
