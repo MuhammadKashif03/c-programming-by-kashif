6a5f97e489d20499b2e5a608f0f9cbf6dca1c85d
Muhammad Kashif <132649154+MuhammadKashif03@users.noreply.github.com>
2023-10-13 15:48:34 +0500

    c program to print a sentense

--- app.c
+++ app.c
@@ -1,29 +1,8 @@
 #include <stdio.h>
 int main()
 {
+    //c programe to print a sentense
 
-    //working of assignment operators
-    int a =5, c;
-
-    //assignment operator
-    c=a;
-    printf("c is : %d", c);  // c is 5
-
-    c +=a;
-    printf("c is : %d", c); // c is 10
-
-    c-=a;
-    printf("c is : %d", c); // c is 5
-
-    c*=a;
-    printf("c is : %d", c); // c is 25
-
-    c/=a;
-    printf("c is : %d", c); // c is 5
-
-    c%=a;
-    printf("c is: %d", c); // c is 0
-
-    return 0;
-
+    printf("i am Muhammad kashif. i am embedded systems engineer.\n");
+    return 0 ;
 }
