090c5a88106e9bbd91099154c7b4dc0b353e269d
Muhammad Kashif <132649154+MuhammadKashif03@users.noreply.github.com>
2023-10-17 11:15:10 +0500

    c program to add two integer entered by the user

--- app.c
+++ app.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 int main()
 {
-    //c programe to print an integer entered by the user
-    int a;
-    printf("enter an integer value:");
-    scanf("%d",&a);
-    printf("entered integer value is: %d", a);
-    return 0;
+   //c programe to add two integers entered by the user
+   int a ,b,c ;
+   printf("enter two numbers to add them: \n");
+   scanf("%d%d",&a,&b);
+   c = a + b;
+   printf("sum is: %d",c);
+   return 0;
 }
