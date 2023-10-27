11b58df2a602c8972d38ceb1d110a1e3715785c7
Muhammad Kashif <132649154+MuhammadKashif03@users.noreply.github.com>
2023-10-27 11:51:56 +0500

    Array input and output

--- app.c
+++ app.c
@@ -1,22 +1,18 @@
 #include <stdio.h>
-int addNumber(int a , int b); //function prototype
-
 int main()
 {
-   int sum, num1, num2;
+  int array[5];
+  printf("enter integer array:\n");
+  for(int i =0; i<5; ++i)
+  {
+    scanf("%d", &array[i]);
+  }
 
-   printf("enter two number: ");
-   scanf("%d %d", &num1, &num2);
-
-   sum = addNumber(num1,num2); //call function
-   printf("sum is %d", sum);
-   return 0;
+  //display the array
+  printf("display the array\n");
+  for(int i =0 ; i<5; ++i)
+  {
+    printf("%d\n",array[i]);
+  }
+ return 0;
 }
-
-int addNumber(int a , int b)   //funtion statement
-{
-  int result;
-  result = a + b;
-  return result;    //return statement
-}
-
